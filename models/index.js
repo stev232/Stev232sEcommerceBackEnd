e21051df4ccca524da2bc1391edab49a4e9158c5
stev232 <stev232@yahoo.com>
2023-03-06 07:36:36 -0600

    Fixed error where delete feature would cause an endless load. Also corrected the put feature so that database is properly updated with the new information.

--- models/index.js
+++ models/index.js
@@ -5,16 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.hasOne(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
-Category.hasMany(Product);
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag);
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+  through: 'product_tag',
+  onDelete: 'CASCADE'
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag);
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_id',
+  through: 'product_tag',
+  onDelete: 'CASCADE'
+});
 
 module.exports = {
   Product,
